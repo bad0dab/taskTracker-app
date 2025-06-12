8695f0a0585a79e8ca0ee000d6cf8b633534a653
bad0dab <broadnicegamerr@gmail.com>
2025-06-12 19:54:01 +0200

    create task table, create .env and changed port, placeholder rows

--- taskTracker-app/src/main.tsx
+++ taskTracker-app/src/main.tsx
@@ -2,9 +2,42 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function CreateTaskTable() {
+  // placeholders
+  let taskNumber = 1; 
+  let taskName = "Placeholder Task 1";
+  let taskStatus = "Pending";
+  let taskTimeAdded = "09:00 AM";
+
+  ///////////////////////
+  return (
+    <table className="table custom-table">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Task</th>
+          <th>Status</th>
+          <th>Time Added</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{taskNumber}</td>
+          <td>{taskName}</td>
+          <td>{taskStatus}</td>
+          <td>{taskTimeAdded}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <CreateTaskTable />
   </StrictMode>,
 )
